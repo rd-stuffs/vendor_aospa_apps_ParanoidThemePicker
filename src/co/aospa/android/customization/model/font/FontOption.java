f42f9a1065fb0d029afe641d16a7c11f1c373222
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-02 18:56:29 +0530

    ParanoidThemePicker: Update font and icon picker UI
    Adapt to the new colors in U-QPR1.
    
    Change-Id: I9bd984a74f54848632df0b23e5333e22f27008be

--- src/co/aospa/android/customization/model/font/FontOption.java
+++ src/co/aospa/android/customization/model/font/FontOption.java
@@ -58,10 +58,13 @@ public class FontOption implements CustomizationOption<FontOption> {
         Resources res = view.getContext().getResources();
         ((TextView) view.findViewById(R.id.thumbnail_text)).setTypeface(
                 mHeadlineFont);
-        int colorFilter = ResourceUtils.getColorAttr(view.getContext(),
-                view.isActivated() || view.getId() == R.id.font_section_tile
-                        ? android.R.attr.textColorPrimary
-                        : android.R.attr.textColorTertiary);
+        if (view.getId() == R.id.font_section_tile) {
+            return;
+        }
+        int colorFilter = view.getContext().getResources().getColor(
+                view.isActivated()
+                        ? R.color.system_on_surface
+                        : R.color.system_on_surface_variant);
         ((TextView) view.findViewById(R.id.thumbnail_text)).setTextColor(colorFilter);
         view.setContentDescription(mTitle);
     }
